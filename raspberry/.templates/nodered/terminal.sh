487ebf2fd9e0c8f4638c16cbf596eb956d984246
root <hedi.sfaxi02@gmail.com>
2024-05-17 15:55:30 +0200

    configuration mqtt nodered

--- raspberry/.templates/nodered/terminal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'to generate a new password hash copy the following line and change PASSWORD'
-echo $'node -e "console.log(require(\'bcryptjs\').hashSync(process.argv[1], 8));" PASSWORD'
-echo 'then "exit"'
-
-docker exec -it nodered bash
