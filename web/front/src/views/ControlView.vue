15ac760196589eae6e346548cab17c2b45393d8d
augusto-moraes <moraestf.augusto@gmail.com>
2024-05-24 17:27:20 +0200

    added controlview

--- web/front/src/views/ControlView.vue
+++ web/front/src/views/ControlView.vue
@@ -1,5 +1,46 @@
 <template>
-    <div>
-        
+    <div class="grid-container">
+        <div class="center" style="margin: 30px;">
+            <v-btn v-for="action in actions" :key="action" @click="handleButtonClick(action.value)" style="margin-right: 10px;">
+                <v-icon left>{{ action.icon }}</v-icon>
+                {{ action.value }}
+            </v-btn>
+        </div>
+        <div class="center" style="margin: 10px;">
+            <!-- Renders an image element with a live image of the terrarium (updated every half hour?) -->
+            <img :src="imageSrc" alt="Terrarium Live Image" style="width: 500px;">
+        </div> 
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            imageSrc: "https://img-3.journaldesfemmes.fr/55Pa2VVqjc0hXSevl8ddLxHV53Y=/1500x/smart/6f75f95c0d54470fa206aa78fe6ed3a8/ccmcms-jdf/39925288.jpg", // Replace with your image URL
+            actions: [
+                {icon: 'mdi-water', value: this.$t('controlView.irrigate')}, 
+                {icon: 'mdi-lightbulb', value: this.$t('controlView.light')}
+            ]
+        };
+    },
+    methods: {
+        handleButtonClick(action) {
+            // Handle button click event here
+            console.log('Button clicked:', action);
+        }
+    }
+};
+</script>
+
+<style scoped>
+    .grid-container {
+        display: grid;
+        justify-content: center;
+    }
+
+    .center {
+        text-align: center;
+    }
+/* Add your custom styles here */
+</style>
