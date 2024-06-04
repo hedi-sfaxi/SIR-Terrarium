4dfb813175dd1bc8f7a9a36778651c0ea70eebbf
Gausse <dkgm12@yahoo.com>
2024-06-04 15:51:11 +0200

    added Hedi's scripts to the middleware

--- web/middleware/app/analysis.py
+++ web/middleware/app/analysis.py
@@ -1,1 +1,18 @@
-# TODO add here the code for correlation analysis and rupture detection
+import logging
+from influxdb import InfluxDBClient
+from utils import fetch_sensors_data, insert_correlation_to_influxdb, calculate_correlation, insert_ruptures_to_influxdb, analyze_ruptures
+
+def correlation(client : InfluxDBClient, param1: str, param2: str):
+    data = fetch_sensors_data(client, [param1, param2])
+    correlation_data = calculate_correlation(data[param1], data[param2])
+    insert_correlation_to_influxdb(data['time'], correlation_data, f"{param1}_{param2}_correlation") #TODO: manage measurement name
+
+    logging.info("Correlation data has been written to InfluxDB.")
+
+def ruptures(client : InfluxDBClient, params: list):
+    data = fetch_sensors_data(client, params)
+    for param in params:
+        rupture_points = analyze_ruptures(data[param])
+        insert_ruptures_to_influxdb(data['time'], rupture_points, f"{param}_ruptures")
+
+    logging.info("Rupture data has been written to InfluxDB.")
--- web/middleware/app/main.py
+++ web/middleware/app/main.py
@@ -1,1 +1,41 @@
-# TODO add here the code for a web rest api
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from influxdb import InfluxDBClient
+from analysis import correlation, ruptures
+
+
+app = FastAPI()
+influxDbClient = InfluxDBClient(host='localhost', port=8086)
+AUTHORIZED_COLUMNS = ['temperature', 'humidity', 'light', 'moisture']
+
+@app.get("/")
+def get_root():
+    return {"message": "Welcome to the middleware server!"}
+
+@app.get("/irrigate")
+def get_irrigate():
+    # send irrigation control via mqtt
+    return {"message": "The plants are being irrigated!"}
+
+@app.get("/ruptures")
+def get_rupture():
+    # launch rupture analysis
+    ruptures(AUTHORIZED_COLUMNS)
+
+    return {"message": "Rupture analysis completed"}
+
+@app.get("/correlation")
+def get_correlation(param1 : str, param2 : str):
+    # raise and error if the param is not authorized
+    for param in [param1, param2]:
+        if param not in AUTHORIZED_COLUMNS:
+            raise HTTPException(status_code=401, detail="Unauthorized column")
+    
+    # launch correlation analysis
+    correlation(influxDbClient, param1, param2)
+
+    return {"message": "Correlation analysis completed"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8082, reload=True)
