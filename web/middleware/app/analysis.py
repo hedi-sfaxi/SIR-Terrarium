4ec1d8ebfc68863ab2f3528cab0cc2d47dfd8ba6
Gausse <dkgm12@yahoo.com>
2024-06-12 19:02:47 +0200

    fixes on middleware

--- web/middleware/app/analysis.py
+++ web/middleware/app/analysis.py
@@ -5,7 +5,7 @@ from utils import fetch_sensors_data, insert_correlation_to_influxdb, calculate_
 def correlation(client : InfluxDBClient, param1: str, param2: str):
     data = fetch_sensors_data(client, [param1, param2])
     correlation_data = calculate_correlation(data[param1], data[param2])
-    insert_correlation_to_influxdb(data['time'], correlation_data, f"{param1}_{param2}_correlation") #TODO: manage measurement name
+    insert_correlation_to_influxdb(client, data['time'], correlation_data, f"{param1}_{param2}_correlation") #TODO: manage measurement name
 
     logging.info("Correlation data has been written to InfluxDB.")
 
@@ -13,6 +13,6 @@ def ruptures(client : InfluxDBClient, params: list):
     data = fetch_sensors_data(client, params)
     for param in params:
         rupture_points = analyze_ruptures(data[param])
-        insert_ruptures_to_influxdb(data['time'], rupture_points, f"{param}_ruptures")
+        insert_ruptures_to_influxdb(client, data['time'], rupture_points, f"{param}_ruptures")
 
     logging.info("Rupture data has been written to InfluxDB.")
