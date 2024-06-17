080a16820d43356ede99216803e41c238a07766a
Gausse <dkgm12@yahoo.com>
2024-06-17 19:45:44 +0200

    control panel

--- web/front/src/main.js
+++ web/front/src/main.js
@@ -4,9 +4,13 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import i18n from './plugins/i18n'
 import router from './router'
+import store from './store'
 
 loadFonts()
 
-createApp(App).use(router).use(i18n)
+createApp(App)
+  .use(router)
+  .use(i18n)
   .use(vuetify)
+  .use(store)
   .mount('#app')
